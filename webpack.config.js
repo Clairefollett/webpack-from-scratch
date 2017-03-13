c8f0e00c471b029bf2d5229b2c15b0cee4a4b8f3
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-13 16:38:50 -0700

    adds htmlwebpackplugin adds index.html htmlwebpackplugin makes index.html in build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+
  module.exports = {
      entry: './src/index.js',
      output: {
          path: './build',
          filename: 'bundle.js'
-     }
+     },
+     plugins: [
+         new HtmlWebpackPlugin({
+             template: './src/index.html'
+         })
+     ]
  };
