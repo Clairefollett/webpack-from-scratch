c2f65b49c333d69f16c0ed309575eee84ef4c855
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-14 10:13:11 -0700

    added html plugin and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         path: './build',
         filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }]
     }
-}
+};
