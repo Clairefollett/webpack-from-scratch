b23cc5005c6db30e7659797b4d61c94844ad2c2a
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-14 16:50:52 -0700

    adds Footer and its scss

---
+++ src/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <h1>sitting at the bottom of the page...</h1>
+        </footer>
+    );
+}
+
+export default Footer;
