af937b118df5f20d1992fab1630bc1defe07d5d3
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-15 01:26:10 -0700

    fiddles with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,99 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-import path from 'path';
+const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+        path.join(__dirname, 'src/index.js'),
+    ],
     output: {
-        path: './build',
-        filename: 'bundle.js'
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
     },
     plugins: [
         new HtmlWebpackPlugin({
-            // filename: 'index.html',
+            filename: 'index.html',
             template: './src/index.html'
-        })
+        }),
     ],
     module: {
-        rules: [{
-            enforce: 'pre',
-            test: /\.js$/,
-            loader: 'eslint-loader',
-            exclude: /node_modules/
-        }, {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            options: { presets: ['es2015'] }
-        }
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js?$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            }, {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                // options: { presets: ['es2015'] }
+            }, {
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader!sass-loader'
+            }
         ]
     }
 };
+
+// export default () => ({
+//     entry: [
+//         path.join(__dirname, 'src/index.js'),
+//     ],
+//     output: {
+//         path: path.join(__dirname, 'build'),
+//         filename: 'bundle.js',
+//     },
+//     plugins: [
+//         new HtmlWebpackPlugin({
+//             filename: 'index.html',
+//             template: './src/index.html'
+//         }),
+//     ],
+//     module: {
+//         rules: [
+//             {
+//                 enforce: 'pre',
+//                 test: /\.js?$/,
+//                 loader: 'eslint-loader',
+//                 exclude: /node_modules/
+//             }, {
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//                 loader: 'babel-loader',
+//                 options: { presets: ['es2015'] }
+//             }, {
+//                 test: /\.scss$/,
+//                 loader: 'style-loader!css-loader!sass-loader'
+//             }
+//         ]
+//     }
+
+// });
+
+// module.exports = {
+//     entry: './src/index.js',
+//     output: {
+//         path: './build',
+//         filename: 'bundle.js'
+//     },
+//     plugins: [
+//         new HtmlWebpackPlugin({
+//             // filename: 'index.html',
+//             template: './src/index.html'
+//         })
+//     ],
+//     module: {
+//         rules: [{
+//             enforce: 'pre',
+//             test: /\.js$/,
+//             loader: 'eslint-loader',
+//             exclude: /node_modules/
+//         }, {
+//             test: /\.js$/,
+//             exclude: /node_modules/,
+//             loader: 'babel-loader',
+//             options: { presets: ['es2015'] }
+//         }
+//         ]
+//     }
+// };
