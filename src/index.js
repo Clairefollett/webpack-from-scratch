5b34a8588a37e1c5d61fe3ff5e144e0e5330ece3
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-14 17:09:38 -0700

    updates index files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-// const foo = () => {};
-import foo from './foo';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const bar = 'bar';
+import App from './components/app/App';
 
-[1, 2, 3].map( x => x*2);
-console.log(`this is ${bar} ${foo}`);
+import './index.scss';
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
