a729265760402dcc1a996f7a02528b8f1cba0d10
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-14 17:10:29 -0700

    updates webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
 
- module.exports = {
-     entry: './src/index.js',
-     output: {
-         path: './build',
-         filename: 'bundle.js'
-     },
-     plugins: [
-         new HtmlWebpackPlugin({
-             template: './src/index.html'
-         })
-     ],
-     module: {
-         rules: [{
-             enforce: 'pre',
-             test: /\.js$/,
-             loader: 'eslint-loader',
-             exclude: /node_modules/
-         }
-         ,{
-             test: /\.js$/,
-             exclude: /node_modules/,
-             loader: 'babel-loader',
-             options: {presets: ['es2015']}
-         }
-         ]
-     }
- };
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: './build',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [{
+            enforce: 'pre',
+            test: /\.js$/,
+            loader: 'eslint-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: { presets: ['es2015'] }
+        }
+        ]
+    }
+};
